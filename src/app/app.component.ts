8b484db0e80da27d6ba27d843b03e338609340f2
Johan <johanmamani@upeu.edu.pe>
2019-08-14 20:58:11 -0500

    hola

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Angular1';
+  constructor(private router:Router){}
+  Menu1(){
+    this.router.navigate(["menu1"]);
+  }
+  Menu2(){
+    this.router.navigate(["menu2"]);
+  }
+  Menu3(){
+    this.router.navigate(["menu3"]);
+  }
+}
